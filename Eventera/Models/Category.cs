9fe01ba8a9209d0f913ace51af5713d3bd07e5d2
Dan Shaw <dcshaw.en@gmail.com>
2025-09-19 16:15:02 -0300

    Adds README.md

---
+++ Eventera/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Eventera.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public List<Event>? Events { get; set; }
+    }
+}
---
+++ Eventera/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Eventera.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public DateTime EventTime { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
