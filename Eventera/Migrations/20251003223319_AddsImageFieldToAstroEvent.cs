3a89448ce32a061c7a368c43813743e2c810b3d2
Dan Shaw <dcshaw.en@gmail.com>
2025-10-03 20:18:13 -0300

    Stages Eventera Phase 1 for code review

---
+++ Eventera/Migrations/20251003223319_AddsImageFieldToAstroEvent.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eventera.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddsImageFieldToAstroEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "imageUrl",
+                table: "AstronomicalEvent",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "imageUrl",
+                table: "AstronomicalEvent");
+        }
+    }
+}
--- Eventera/Migrations/20251003223458_AddsImageFieldToAstroEvent2.Designer.cs
+++ Eventera/Migrations/20251003223458_AddsImageFieldToAstroEvent2.Designer.cs
@@ -4,6 +4,7 @@ using Eventera.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Eventera.Migrations
 {
     [DbContext(typeof(EventeraContext))]
-    partial class EventeraContextModelSnapshot : ModelSnapshot
+    [Migration("20251003223458_AddsImageFieldToAstroEvent2")]
+    partial class AddsImageFieldToAstroEvent2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
