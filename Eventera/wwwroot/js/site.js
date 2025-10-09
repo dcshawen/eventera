f86a0e2437b8bd301142c197e4a37c98be31ee48
Dan Shaw <dcshaw.en@gmail.com>
2025-10-09 15:38:02 -0300

    Adds styling and minor changes

--- Eventera/wwwroot/js/site.js
+++ Eventera/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+(() => {
+    document.getElementById('toggleTheme').addEventListener('click', function (e) {
+        e.preventDefault();
+        var htmlTag = document.documentElement;
+        var currentTheme = htmlTag.getAttribute('data-bs-theme');
+        htmlTag.setAttribute('data-bs-theme', currentTheme === 'dark' ? 'light' : 'dark');
+    });
+})();
