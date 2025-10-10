4bfad113663a9fc4c7722f19c5d156a867031445
Dan Shaw <dcshaw.en@gmail.com>
2025-10-10 20:46:47 -0300

    Adds Event Image

--- Eventera/Migrations/20251010224654_AddsImage.Designer.cs
+++ Eventera/Migrations/20251010224654_AddsImage.Designer.cs
@@ -4,6 +4,7 @@ using Eventera.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Eventera.Migrations
 {
     [DbContext(typeof(EventeraContext))]
-    partial class EventeraContextModelSnapshot : ModelSnapshot
+    [Migration("20251010224654_AddsImage")]
+    partial class AddsImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -40,8 +43,7 @@ namespace Eventera.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Filename")
-                        .IsRequired()
+                    b.Property<string>("ImageUrl")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Location")
--- Eventera/Migrations/20251010225424_AddsFilename.Designer.cs
+++ Eventera/Migrations/20251010225424_AddsFilename.Designer.cs
@@ -4,6 +4,7 @@ using Eventera.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Eventera.Migrations
 {
     [DbContext(typeof(EventeraContext))]
-    partial class EventeraContextModelSnapshot : ModelSnapshot
+    [Migration("20251010225424_AddsFilename")]
+    partial class AddsFilename
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -42,6 +45,9 @@ namespace Eventera.Migrations
 
                     b.Property<string>("Filename")
                         .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("ImageUrl")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Location")
