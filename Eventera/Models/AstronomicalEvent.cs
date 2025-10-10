17edb0128f4b38db2cd12ffa7b074007a26d4abd
Dan Shaw <dcshaw.en@gmail.com>
2025-10-10 20:46:47 -0300

    Adds Event Image

--- Eventera/Models/AstronomicalEvent.cs
+++ Eventera/Models/AstronomicalEvent.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
 
 namespace Eventera.Models
@@ -10,15 +11,18 @@ namespace Eventera.Models
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Location { get; set; } = string.Empty;
+        public string Filename { get; set; } = string.Empty;
         [Display(Name = "Start Date")]
         public DateTime StartDateTime { get; set; }
         [Display(Name = "Created")]
         public DateTime CreatedDateTime { get; set; }
-        [Display(Name = "Image")]
-        public string? ImageUrl { get; set; } = string.Empty;
         [Display(Name = "Category")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Image File")]
+        public IFormFile? ImageFile { get; set; }
 
         public AstronomicalEvent()
         {
